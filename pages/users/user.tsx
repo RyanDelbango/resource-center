b602264dc57a937bb770a5ed7f0fc8679ad6d7a8
Ryan Delbango <Ryan.Delbango@gmail.com>
2022-11-03 16:52:19 -0400

    Add: bootstrap styling

--- pages/users/user.tsx
+++ pages/users/user.tsx
@@ -1,7 +1,16 @@
+import Link from 'next/link';
+
 const user = (
-    <h1>
-        Timothy Jones
-    </h1>
+    <div className="card" style={{width: 300}}>
+        <div className="card-body">
+            <h1 className="card-title">
+                Timothy Jones
+            </h1>
+            <h6 className="card-subtitle mb-2 text-muted">
+                <Link href="/">Beware</Link>
+            </h6>
+        </div>
+    </div>
 )
 
 const FirstUser = () => {
