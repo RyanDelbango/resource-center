d2f6f7d64c26414d96e0909538de9386f3331542
Ryan Delbango <Ryan.Delbango@gmail.com>
2022-11-12 16:18:11 -0500

    Refactor: move inline style to dedicated css file

--- pages/users/user.tsx
+++ pages/users/user.tsx
@@ -1,15 +1,13 @@
 import Link from 'next/link';
 
 const user = (
-    <div className="card" style={{width: 300}}>
-        <div className="card-body">
-            <h1 className="card-title">
-                Timothy Jones
-            </h1>
-            <h6 className="card-subtitle mb-2 text-muted">
-                <Link href="/">Beware</Link>
-            </h6>
-        </div>
+    <div className="card-body">
+         <h1  className="card-title">
+            Timothy Jones
+        </h1>
+        <h6 className="card-subtitle mb-2 text-muted">
+            <Link href="/">Beware</Link>
+        </h6>
     </div>
 )
 
